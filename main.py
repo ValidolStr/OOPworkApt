52c9d43eb8656d81019879e003e0b8e9b14fd52a
ValidolStr <chizhewskyvadim@gmail.com>
2024-05-07 12:30:32 +0300

    second window

--- main.py
+++ main.py
@@ -1,41 +1,37 @@
 import customtkinter
 from customtkinter import *
+import subprocess
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-green"
 
-app = customtkinter.CTk()  # create CTk window like the Tk() constructor
-app.geometry("400x200")
-app.title("Apteka.BY")
+correct_login = "user"
+correct_password = "password"
 
-correct_login = "12"
-correct_password = "12"
+
+def open_second_window():
+    first_window.destroy()
+    second_window = customtkinter.CTk()
+    second_window.geometry("400x200")
+    second_window.title("Second Window")
+    second_window.mainloop()
+
+    label = customtkinter.CTkLabel(master=second_window, text="Успех!", width=200, height=100)
+    label.pack(pady=20)
 
 
 def login_command():
     if entry_1.get() == correct_login and entry_2.get() == correct_password:
-        new_window = customtkinter.CTk()
-        new_window.geometry("200x100")
-        new_window.title("Login Window")
-        label_3 = customtkinter.CTkLabel(master=new_window, text="Complited", width=120, height=25)
-        label_3.pack(pady=20)
+        open_second_window()
     else:
         print("Incorrect login or password")
 
 
-def register_command():
-    new_window = customtkinter.CTk()
-    new_window.geometry("200x100")
-    new_window.title("Registration Window")
+first_window = customtkinter.CTk()
+first_window.geometry("400x200")
+first_window.title("Apteka.BY")
 
-    def save_command():
-        print("Save button clicked")
-
-    button_3 = customtkinter.CTkButton(master=new_window, text="СОХРАНИТЬ", command=save_command)
-    button_3.pack(pady=20)
-
-
-frame_1 = customtkinter.CTkFrame(master=app)
+frame_1 = customtkinter.CTkFrame(master=first_window)
 frame_1.pack(pady=20, padx=20)
 
 label_1 = customtkinter.CTkLabel(master=frame_1, text="Login", width=120, height=25)
@@ -53,7 +49,7 @@ entry_2.grid(row=3, column=0, columnspan=2)
 button_1 = customtkinter.CTkButton(master=frame_1, text="Vhod", command=login_command)
 button_1.grid(row=4, column=0, pady=10, padx=10)
 
-button_2 = customtkinter.CTkButton(master=frame_1, text="Registracia", command=register_command)
+button_2 = customtkinter.CTkButton(master=frame_1, text="Registracia", command=open_second_window)
 button_2.grid(row=4, column=1, pady=10, padx=10)
 
-app.mainloop()
+first_window.mainloop()
