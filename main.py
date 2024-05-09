7402bd28bc6b2f1469d2f636105feb31e643107c
ValidolStr <chizhewskyvadim@gmail.com>
2024-05-09 15:16:10 +0300

    Autorise

--- main.py
+++ main.py
@@ -1,74 +1,109 @@
+import flet as ft
 import sqlite3
-import customtkinter
-from customtkinter import *
 
-
-customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-green"
-
-correct_login = "user"
-correct_password = "password"
-
-connection = sqlite3.connect("apteka.db")  # Подставьте имя вашей базы данных SQLite
-cursor = connection.cursor()
-
-connection.commit()
-
-def open_second_window():
-    first_window.destroy()
-    second_window = customtkinter.CTk()
-    second_window.geometry("400x200")
-    second_window.title("Second Window")
-    second_window.mainloop()
-
-    label = customtkinter.CTkLabel(master=second_window, text="Успех!", width=200, height=100)
-    label.pack(pady=20)
-
-
-def login_command():
-    username = entry_1.get()
-    password = entry_2.get()
-
-    # Выполняем SQL-запрос для выборки записи из таблицы "users" с заданным логином и паролем
-    cursor.execute("""
-        SELECT * FROM users WHERE login = ? AND password = ?
-    """, (username, password))
+def main (page: ft.Page):
+    page.title = "Apteka.By"
+    page.theme_mode = 'dark'
+    page.vertical_alignment = ft.MainAxisAlignment
+    page.window_width = 500
+    page.window_height = 500
+    page.window_resizable = False
     
-    print(cursor.fetchall())
+    def regisr (e):
+        db = sqlite3.connect('apteka.db')
+        cr = db.cursor()
+        cr.execute("""CREATE TABLE IF NOT EXISTS users(
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       login TEXT,
+                       password TEXT
+                       )""")
+        cr.execute(f"INSERT INTO users VALUES(null, '{userlog.value}', '{userpass.value}')")
+        db.commit()
+        db.close()
+        
+        userlog.value = ''
+        userpass.value = ''
+        btnreg.text = 'Добавлено'
+        page.update()
 
-    # Получаем результат запроса
-    user = cursor.fetchone()
 
-    if user:
-        open_second_window()
-        connection.close()
-    else:
-        print("Incorrect login or password")
-        connection.close()
+    def valid (e):
+        if all([userlog.value, userpass.value]):
+         btnreg.disabled = False
+         btnauth.disabled = False
+        else:
+         btnreg.disabled = True
+         btnreg.disabled= True
+        
+        page.update()
+        
+        
+    def auth (e):
+        db = sqlite3.connect('apteka.db')
+        cr = db.cursor()
+        
+        cr.execute(f"SELECT * FROM users WHERE login = '{userlog.value}'AND password = '{userpass.value}'")
+        if cr.fetchone() != None:
+            userlog.value = ''
+            userpass.value = ''
+            btnauth.text = 'Войдено'
+            page.update()
+        else:
+            page.snack_bar = ft.SnackBar(ft.Text('Неверные данные!'))
+            page.snack_bar.open = True
+            userlog.value = ''
+            userpass.value = ''
+            page.update()
+            
+        
+        db.commit()
+        db.close()
+    
+    userlog = ft.TextField(label='Логин', on_change=valid)
+    userpass = ft.TextField(label='Пароль', password=True, on_change=valid)
+    btnreg = ft.OutlinedButton(text='Добавить', width=200, on_click=regisr, disabled=True )   
+    btnauth = ft.OutlinedButton(text='Вход', width=200, on_click=auth, disabled=True )   
 
-first_window = customtkinter.CTk()
-first_window.geometry("400x200")
-first_window.title("Apteka.BY")
-
-frame_1 = customtkinter.CTkFrame(master=first_window)
-frame_1.pack(pady=20, padx=20)
-
-label_1 = customtkinter.CTkLabel(master=frame_1, text="Login", width=120, height=25)
-label_1.grid(row=0, column=0, columnspan=2)
-
-entry_1 = customtkinter.CTkEntry(master=frame_1, width=200, height=25)
-entry_1.grid(row=1, column=0, columnspan=2)
-
-label_2 = customtkinter.CTkLabel(master=frame_1, text="Parol", width=120, height=25)
-label_2.grid(row=2, column=0, columnspan=2)
-
-entry_2 = customtkinter.CTkEntry(master=frame_1, width=200, height=25)
-entry_2.grid(row=3, column=0, columnspan=2)
-
-button_1 = customtkinter.CTkButton(master=frame_1, text="Vhod", command=login_command)
-button_1.grid(row=4, column=0, pady=10, padx=10)
-
-button_2 = customtkinter.CTkButton(master=frame_1, text="Registracia", command=open_second_window)
-button_2.grid(row=4, column=1, pady=10, padx=10)
-
-first_window.mainloop()
+    panel_reg = ft.Row([
+            ft.Column([
+                ft.Text("Регистрация"),
+                userlog,
+                userpass,
+                btnreg     ]      
+                )
+        ],
+        alignment=ft.MainAxisAlignment.CENTER  
+            
+    )
+    
+    panel_autorise = ft.Row([
+            ft.Column([
+                ft.Text("Авторизация"),
+                userlog,
+                userpass,
+                btnauth     ]      
+                )
+        ],
+        alignment=ft.MainAxisAlignment.CENTER  
+            
+    )
+    
+    def navigator (e):
+        i = page.navigation_bar.selected_index
+        page.clean()
+        
+        if i == 0:  page.add(panel_reg)
+        elif i ==1: page.add(panel_autorise)
+            
+    
+    page.navigation_bar = ft.NavigationBar(
+        destinations=[
+            ft.NavigationDestination(icon=ft.icons.VERIFIED_USER, label="Регистрация"),
+            ft.NavigationDestination(icon=ft.icons.VERIFIED_USER_OUTLINED, label="Авторизация")
+        ], on_change=navigator
+    )
+    
+    page.add(panel_reg)
+    
+ft.app(target=main)
+    
